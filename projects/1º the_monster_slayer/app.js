d263e87cade8825c398da96ef2aebe9c9e79471d
Jhoni Faber <jhonatanfaber@gmail.com>
2018-09-07 11:53:34 +0200

    set maximum healthbar

--- projects/1º the_monster_slayer/app.js
+++ projects/1º the_monster_slayer/app.js
@@ -3,8 +3,8 @@ new Vue({
     data: {
         playerLife: 100,
         monsterLife: 100,
-        MONSTERMESSAGE : "Monster hits player for ",
-        PLAYERMESSAGE : "Player hits monster for ",
+        MONSTERMESSAGE: "Monster hits player for ",
+        PLAYERMESSAGE: "Player hits monster for ",
         startGame: true,
         showControls: false,
         showLog: false,
@@ -22,45 +22,54 @@ new Vue({
         startNewGame() {
             this.startGame = false
             this.showControls = true
-        },
-        giveUpMatch() {
-            this.showControls = false
-            this.startGame = true
-            this.showLog = false
             this.playerLife = 100
             this.monsterLife = 100
             this.log = []
         },
+        giveUpMatch() {
+            this.startGame = true
+            this.showControls = false
+            this.showLog = false
+        },
         attack() {
             this.showLog = true
-
             let monsterHit = this.generateRandomNumber(4, 5)
-            this.playerLife = this.playerLife - monsterHit
-            this.saveLogMessage(this.MONSTERMESSAGE,monsterHit)
-
+            this.playerLife -= monsterHit
+            this.saveLogMessage(this.MONSTERMESSAGE, monsterHit)
+            if(this.checkForWin(this.playerLife)){
+                this.playerLife = 0
+                return 
+            } 
+                
             let playerHit = this.generateRandomNumber(5, 5)
-            this.monsterLife = this.monsterLife - playerHit
-            this.saveLogMessage(this.PLAYERMESSAGE,playerHit)
+            this.monsterLife -= playerHit
+            this.saveLogMessage(this.PLAYERMESSAGE, playerHit)
+            if(this.checkForWin(this.monsterLife)) this.monsterLife = 0
         },
         specialAttack() {
             this.showLog = true
-
             let monsterHit = this.generateRandomNumber(5, 4)
-            this.playerLife = this.playerLife - monsterHit
-            this.saveLogMessage(this.MONSTERMESSAGE,monsterHit)
+            this.playerLife -= monsterHit
+            this.saveLogMessage(this.MONSTERMESSAGE, monsterHit)
+            if(this.checkForWin(this.playerLife)){
+                this.playerLife = 0
+                return 
+            }
 
             let playerHit = this.generateRandomNumber(6, 8)
-            this.monsterLife = this.monsterLife - playerHit
-            this.saveLogMessage(this.PLAYERMESSAGE,playerHit)
+            this.monsterLife -= playerHit
+            this.saveLogMessage(this.PLAYERMESSAGE, playerHit)
+            if(this.checkForWin(this.monsterLife)) this.monsterLife = 0
         },
         heal() {
             let monsterHit = this.generateRandomNumber(2, 2)
-            this.playerLife = this.playerLife - monsterHit
-            this.saveLogMessage(this.MONSTERMESSAGE,monsterHit)
+            this.playerLife -= monsterHit
+            this.saveLogMessage(this.MONSTERMESSAGE, monsterHit)
 
             let playerHealing = this.generateRandomNumber(5, 4)
-            this.playerLife = this.playerLife + playerHealing
-            this.saveLogMessage("PLAYER HEALS HIMSELF FOR ",playerHealing)
+            this.playerLife += playerHealing
+            this.saveLogMessage("PLAYER HEALS HIMSELF FOR ", playerHealing)
+            if(this.playerLife >= 100) this.playerLife = 100
 
         },
         generateRandomNumber(from, possibleResults) {
@@ -68,6 +77,15 @@ new Vue({
         },
         saveLogMessage(message, hitPoint) {
             this.log.push(message + hitPoint)
+        },
+        checkForWin(player) {
+            if (player <= 0) {
+                this.startGame = true
+                this.showControls = false
+                this.showLog = false
+                return true
+            }
+            return false
         }
     }
 })
