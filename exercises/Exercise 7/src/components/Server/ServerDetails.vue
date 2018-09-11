58247fc238d58465723ebffc14fa05be816adae2
Jhoni Faber <jhonatanfaber@gmail.com>
2018-09-11 10:19:36 +0100

    $emit: communication between siblings using an eventhub or $root option

---
+++ exercises/Exercise 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>{{currentState}}</p>
+    </div>
+
+</template>
+
+<script>
+import { eventBus } from "./../../main.js";
+
+export default {
+  data() {
+    return {
+      currentState: "Server Details are currently not updated"
+    };
+  },
+  created() {
+      // this.$root.$on... also works
+    eventBus.$on("showServerState", data => {
+      this.currentState = data;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ exercises/Exercise 7/src/components/Server/Servers.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="server in servers" :key="server.id"
+                    @click="showServerState(server.state)"
+            >
+                Server #{{ server.id }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {eventBus} from "./../../main.js"
+
+export default {
+
+    data(){
+        return {
+            servers: [
+                {
+                    id : 1, state : "Normal"
+                },
+                {
+                    id : 2, state : "Critical"
+                },
+                {
+                    id : 3, state : "Unknown"
+                },
+                {
+                    id : 4, state : "Off"
+                },
+                {
+                    id : 5, state : "Stopped"
+                }
+            ]
+        }
+    },
+    methods : {
+        showServerState(state){
+            // this.$root.$emit... also works
+            eventBus.$emit("showServerState", state)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
