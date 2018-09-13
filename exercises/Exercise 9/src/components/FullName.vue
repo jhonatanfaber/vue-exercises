d1d8627e1c61795614d7741690beb52214d0056e
Jhoni Faber <jhonatanfaber@gmail.com>
2018-09-13 11:10:01 +0100

    forms and using v-model on components

---
+++ exercises/Exercise 9/src/components/FullName.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="fullName">
+        <p>Full Name: {{firstName}} {{lastName}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    props : ["firstName", "lastName"]
+    
+}
+</script>
+<style scoped>
+
+</style>
+
