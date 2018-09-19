537599408ab91adcf0474f4ef4c94b7fe4652355
Jhoni Faber <jhonatanfaber@gmail.com>
2018-09-19 08:58:07 +0100

    mapgetters

---
+++ exercises/vuex/src/components/AnotherResult.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p> Nº of clicks: {{ clicksCounter }}</p>
+</div>
+</template>
+
+<script>
+import {mapGetters} from "vuex"
+export default {
+  computed: mapGetters([
+    "doubleCounter",
+    "clicksCounter"
+  ]) 
+};
+</script>
